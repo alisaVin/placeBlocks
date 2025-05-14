28d70adaf62e0eced5acabd981b01dcf3be27cde
Alisa Vinogradova <avinogradova@kesslersolutions.de>
2025-05-14 16:13:31 +0200

    initial commit

---
+++ src/BlockDataModel.cs
@@ -0,0 +1,9 @@
+﻿namespace placing_block.src
+{
+    public class BlockDataModel
+    {
+        public string PunktNum { get; set; }
+        public double X { get; set; }
+        public double Y { get; set; }
+    }
+}
