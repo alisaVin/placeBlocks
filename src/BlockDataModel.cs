593bf1f78855c09401c21ec496e85241f430b7ad
Alisa Vinogradova <avinogradova@kesslersolutions.de>
2025-05-15 12:58:31 +0200

    added mvvm stucture and registry function

--- src/BlockDataModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace placing_block.src
-{
-    public class BlockDataModel
-    {
-        public string PunktNum { get; set; }
-        public double X { get; set; }
-        public double Y { get; set; }
-    }
-}
