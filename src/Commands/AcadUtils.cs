eb121a67d49e6130af7945a40c26bd49a3d8cd69
Alisa Vinogradova <avinogradova@kesslersolutions.de>
2025-05-15 13:09:39 +0200

    MVVM structure

---
+++ src/Commands/AcadUtils.cs
@@ -0,0 +1,112 @@
+﻿using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using System;
+using System.IO;
+using acApp = Autodesk.AutoCAD.ApplicationServices.Application;
+
+namespace placing_block.src
+{
+    public class AcadUtils
+    {
+        public static Database OpenDb(string filePath, IReporter reporter)
+        {
+            var db = new Database(false, true);
+            try
+            {
+                db.ReadDwgFile(filePath, FileShare.ReadWrite, true, null);
+                return db;
+            }
+            catch (System.Exception ex)
+            {
+                reporter?.ClearText();
+                if (reporter != null)
+                    reporter.ReportExeption(ex);
+
+                reporter.WriteText($"\nUnable to read drawing file - {filePath}");
+                return null;
+            }
+        }
+
+        public static Document OpenDocument(string sFileName, bool setActive, IReporter reporter)
+        {
+            try
+            {
+                if (!File.Exists(sFileName))
+                    reporter.WriteText($"Datei: {sFileName} ist nicht vorhanden.");
+
+                var docMng = acApp.DocumentManager;
+                Document returnDoc = docMng.Open(sFileName, false);
+                if (setActive)
+                {
+                    if (docMng.MdiActiveDocument != null && returnDoc != docMng.MdiActiveDocument)
+                        docMng.MdiActiveDocument = returnDoc;
+                }
+                return returnDoc;
+            }
+            catch (System.Exception ex)
+            {
+                if (reporter != null)
+                {
+                    reporter.ClearText();
+                    reporter.ReportExeption(ex);
+                    reporter.WriteText($"\nUnable to read drawing file - {sFileName}");
+                }
+                return null;
+            }
+        }
+
+        public static ObjectIdCollection GetBlockDefIds(Database db, IReporter reporter)
+        {
+            ObjectIdCollection newBlDefIds = new ObjectIdCollection();
+            try
+            {
+                Transaction trSource = db.TransactionManager.StartTransaction();
+                using (trSource)
+                {
+                    var bt = trSource.GetObject(db.BlockTableId, OpenMode.ForRead) as BlockTable;
+                    foreach (ObjectId id in bt)
+                    {
+                        var obj = trSource.GetObject(id, OpenMode.ForRead) as DBObject;
+                        var bd = obj as BlockTableRecord;
+                        if ((bd != null) && !bd.IsAnonymous && bd.IsLayout)
+                            newBlDefIds.Add(id);
+                        obj.Dispose();
+                    }
+                    trSource.Commit();
+                    bt.Dispose();
+                }
+            }
+            catch (System.Exception ex)
+            {
+                if (reporter != null)
+                    reporter.ReportExeption(ex);
+            }
+            return newBlDefIds;
+        }
+
+        //Id
+        public static ObjectId GetBlockDef(Database db, string sBlockName)
+        {
+            ObjectId retId = ObjectId.Null;
+            using (Transaction tr = db.TransactionManager.StartTransaction())
+            {
+                var bt = tr.GetObject(db.BlockTableId, OpenMode.ForRead) as BlockTable;
+                foreach (ObjectId id in bt)
+                {
+                    DBObject obj = tr.GetObject(id, OpenMode.ForRead);
+                    var bd = obj as BlockTableRecord;
+                    if (bd == null) continue;
+                    if (String.Compare(bd.Name, sBlockName) == 0)
+                    {
+                        bd.Dispose();
+                        retId = id;
+                        break;
+                    }
+                    bd.Dispose();
+                }
+                bt.Dispose();
+            }
+            return retId;
+        }
+    }
+}
---
+++ src/Commands/Commands.cs
@@ -0,0 +1,196 @@
+﻿using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.Geometry;
+using Autodesk.AutoCAD.Runtime;
+using Microsoft.Win32;
+using placing_block.src;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Reflection;
+using System.Windows.Forms;
+
+
+namespace placing_block
+{
+    public class Commands
+    {
+        Control _ctrl;
+        ExcelReader exReader = new ExcelReader();
+        Reporter _reporter;
+
+        [CommandMethod("PLACEBLOCK", CommandFlags.Session)]
+        //public void Demo()
+        //{
+        //    FormDialog formDlg = new FormDialog { TopMost = false };
+        //    formDlg.Show();
+        //}
+
+
+        //public void PlaceBlocks()
+        public void PlaceBlocks(string blockPath, string coordPath, object sender, DoWorkEventArgs e)
+        {
+            _ctrl = e.Argument as Control;
+            BackgroundWorker bw = sender as BackgroundWorker;
+            if (!File.Exists(coordPath) || !File.Exists(blockPath)) return;
+
+            if (bw.CancellationPending)
+            {
+                e.Cancel = true;
+                return;
+            }
+
+            var dm = Autodesk.AutoCAD.ApplicationServices.Application.DocumentManager;
+            var targetDoc = dm.CurrentDocument;
+            var targetDb = dm.MdiActiveDocument.Database;
+            var ed = dm.MdiActiveDocument.Editor;
+            //Input Feld ///////////////
+            //string blockPath = "C:\\Projects\\Bundesverwaltungsgericht\\placing_block\\Blöcke-techn Anlagen\\FSR.dwg";
+            string blockName = "FSR";
+
+            using (targetDoc.LockDocument(DocumentLockMode.Write, "PLACEBLOCK", "PLACEBLOCK", true))
+            {
+                var blockPoints = exReader.ReadInputData();
+
+                try
+                {
+                    Invoker.Invoke(() =>
+                    {
+                        //mal fragen, ob man DWG-Datei öffnen soll oder nicht (bzw. ohne Öffnung, wie bei copy_file_async)
+
+                        Database sourceDb = AcadUtils.OpenDb(blockPath.StringResult, _reporter);
+                        using (sourceDb)
+                        {
+                            sourceDb.ReadDwgFile(blockPath, FileOpenMode.OpenForReadAndReadShare, true, string.Empty);
+                            if (sourceDb == null) return;
+                            //Input Feld (block name) ///////////////
+                            var blockDefId = AcadUtils.GetBlockDef(sourceDb, blockName);
+                            // Reporter ''''''''''''''''
+                            if (blockDefId == null)
+                            {
+                                ed.WriteMessage("The block doesn't exist in this drawing");
+                                return;
+                            }
+
+                            using (Transaction tr = targetDb.TransactionManager.StartTransaction())
+                            {
+                                var bt = tr.GetObject(targetDb.BlockTableId, OpenMode.ForRead) as BlockTable;
+                                var ms = tr.GetObject(bt[BlockTableRecord.ModelSpace], OpenMode.ForWrite) as BlockTableRecord;
+                                var blIds = new ObjectIdCollection();
+
+                                if (blockDefId != null)
+                                    blIds.Add(blockDefId);
+
+                                var idMapping = new IdMapping();
+                                sourceDb.WblockCloneObjects(blIds, targetDb.BlockTableId, idMapping, DuplicateRecordCloning.Replace, false);
+
+                                //reporter ####################################
+                                ed.WriteMessage($"\nCopied {blIds.Count.ToString()} block definitions from {blockPath} to the current drawing.");
+
+                                List<Point3d> insertPoints = new List<Point3d>(); //Einfhügepunkt - Zentrum
+
+                                foreach (var blPoint in blockPoints)
+                                {
+                                    insertPoints.Add(new Point3d(blPoint.X, blPoint.Y, 0));
+                                }
+
+
+                                var blBtr = AcadUtils.GetBlockDef(targetDb, blockName);
+                                if (blBtr.IsNull) return;
+
+                                for (int i = 0; i < insertPoints.Count; i++)
+                                {
+                                    var newBr = new BlockReference(insertPoints[i], blBtr);
+                                    ms.AppendEntity(newBr);
+                                    tr.AddNewlyCreatedDBObject(newBr, true);
+
+                                    using (var blDef = tr.GetObject(blBtr, OpenMode.ForRead) as BlockTableRecord)
+                                    {
+                                        if (blDef == null || !blDef.HasAttributeDefinitions)
+                                            return;
+
+                                        //attribute auslesen
+                                        if (newBr != null)
+                                        {
+                                            Autodesk.AutoCAD.DatabaseServices.AttributeCollection attrColl = newBr.AttributeCollection;
+                                            foreach (ObjectId adId in blDef)
+                                            {
+                                                var adObj = tr.GetObject(adId, OpenMode.ForRead);
+                                                AttributeDefinition ad = adObj as AttributeDefinition;
+                                                if (ad != null)
+                                                {
+                                                    using (var attrRef = new AttributeReference())
+                                                    {
+                                                        attrRef.SetAttributeFromBlock(ad, newBr.BlockTransform);
+                                                        attrRef.Tag = "Punktkoordinaten";
+                                                        attrRef.TextString = $"{blockPoints[i].X}, {blockPoints[i].Y}";
+                                                        newBr.AttributeCollection.AppendAttribute(attrRef);
+                                                        tr.AddNewlyCreatedDBObject(attrRef, true);
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+
+                                //SwapBlockIds - soll schauen, wie man die Ids von FAMOS zu den Blöcken zuweist
+
+                                ms.Dispose();
+                                tr.Commit();
+                            }
+                        }
+                    }, _ctrl);
+                }
+                catch (Exception ex)
+                {
+                    _reporter.ReportExeption(ex);
+                    ed.WriteMessage($"\n Error during copy: {ex.Message} \n {ex.StackTrace}");
+                }
+            }
+
+        }
+
+        [CommandMethod("RegisterApp", CommandFlags.Session)]
+        public void RegisterApp()
+        {
+            try
+            {
+                string sAppName = "PlacingBlock";
+
+                // Get the AutoCAD Applications key
+                string sProdKey = HostApplicationServices.Current.UserRegistryProductRootKey; //"Software\\Autodesk\\AutoCAD\\R24.1\\ACAD-5101:407"	
+                Microsoft.Win32.RegistryKey regAcadProdKey = Microsoft.Win32.Registry.CurrentUser.OpenSubKey(sProdKey);
+                Microsoft.Win32.RegistryKey regAcadAppKey = regAcadProdKey.OpenSubKey("Applications", true); //{HKEY_CURRENT_USER\Software\Autodesk\AutoCAD\R24.1\ACAD-5101:407}
+
+                // Check to see if the "MyApp" key exists
+                using (regAcadAppKey)
+                {
+                    //var regAppAddInKey = Microsoft.Win32.Registry.CurrentUser.OpenSubKey(regAcadAppKey.ToString()) ?? Microsoft.Win32.Registry.CurrentUser.CreateSubKey(sAppName);
+
+                    // Check to see if the "MyApp" key exists
+                    string[] subKeys = regAcadAppKey.GetSubKeyNames();
+                    foreach (string subKey in subKeys)
+                    {
+                        if (subKey.Equals(sAppName))
+                            return;
+                    }
+
+                    // Get the location of this module
+                    string sAssemblyPath = Assembly.GetExecutingAssembly().Location; //	"C:\\Projects\\my_tasks\\CAD\\dotnet\\copy_files_async\\bin\\x64\\AutoCAD_Debug_2021\\copy_files_async.dll"	
+
+                    // Register the application
+                    Microsoft.Win32.RegistryKey regAppAddInKey = regAcadAppKey.CreateSubKey(sAppName); //{HKEY_CURRENT_USER\Software\Autodesk\AutoCAD\R24.1\ACAD-5101:407\Applications\CopyAsync}
+                    regAppAddInKey.SetValue("DESCRIPTION", sAppName, RegistryValueKind.String);
+                    regAppAddInKey.SetValue("LOADCTRLS", 2, RegistryValueKind.DWord);
+                    regAppAddInKey.SetValue("LOADER", sAssemblyPath, RegistryValueKind.String);
+                    regAppAddInKey.SetValue("MANAGED", 1, RegistryValueKind.DWord);
+                }
+            }
+            catch (System.Exception ex)
+            {
+                _reporter.ReportExeption(ex);
+                MessageBox.Show(ex.Message + "\n" + ex.StackTrace);
+            }
+        }
+    }
+}
